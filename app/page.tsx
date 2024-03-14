8b83c85a684062069243fea468d51f81d19a6034
Josh Mayer <jooshmayer@gmail.com>
2024-03-14 12:44:59 -0700

    minor copy

--- app/page.tsx
+++ app/page.tsx
@@ -16,26 +16,25 @@ export default function Home() {
       <p>
         Websites and complex UI are extra fluff in the emerging world of AI
         agents. We envision an internet where our personal agents can freely
-        roam the internet, answering questions and acting on our behalf. They
-        don't care how buttons look, they want the underlying data presented on
-        the page.
+        roam, answering questions and acting on our behalf. They don't care how
+        buttons look, they want the underlying data presented on the page.
       </p>
       <p>
         Tollbit is aiming to power this application layer of AI. We provide
         scalable and liscensed access to realtime data from primary sources
         meant to perfectly integrate with your inference (or training) time data
         uses like RAG. Stop paying and wasting time scraping. Use Tollbit to go
-        directly to the data instead.
+        directly to the data source instead.
       </p>
       <p>Updates:</p>
       <ul>
         <li>December 2023:</li>
-        <li>October 2023:</li>
+        <li>October 2023: </li>
       </ul>
       <p>Contact us:</p>
       <ul>
         <li>
-          To join us as we build the agent facing layer of the Internet, please
+          To join us as we build the agent facing layer of the internet, please
           email <a href="mailto:team@tollbit.com">team@tollbit.com</a>
         </li>
         <li>
